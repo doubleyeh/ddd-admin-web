70fea0c9093c44b04386a7563c12949a2cd4b2e5
Mosg <869033299@qq.com>
2025-12-11 10:52:06 +0800

    feat:  接口文件简化

--- src/api/system/tenant.ts
+++ src/api/system/tenant.ts
@@ -2,23 +2,31 @@ import { http } from '@/utils/http'
 import type { RestResponse, Page } from '@/types/api'
 import type { TenantCreateResultDTO, TenantDTO, TenantSaveDTO } from '@/types/system/tenant'
 
-export function findPage(page: number, size: number, query: Record<string, any> = {}): Promise<RestResponse<Page<TenantDTO>>> {
+export function findPage(
+  page: number,
+  size: number,
+  query: Record<string, any> = {}
+): Promise<Page<TenantDTO>> {
   const params = {
     page,
     size,
-    ...query
+    ...query,
   }
   return http.get('/tenants', { params })
 }
 
-export function create(data: TenantSaveDTO): Promise<RestResponse<TenantCreateResultDTO>> {
+export function create(data: TenantSaveDTO): Promise<TenantCreateResultDTO> {
   return http.post('/tenants', data)
 }
 
-export function update(id: number, data: TenantSaveDTO): Promise<RestResponse<TenantDTO>> {
+export function update(id: string, data: TenantSaveDTO): Promise<TenantDTO> {
   return http.put(`/tenants/${id}`, data)
 }
 
-export function deleteById(id: number): Promise<RestResponse<void>> {
+export function updateState(id: string, state: Boolean): Promise<TenantDTO> {
+  return http.put(`/tenants/${id}?state=${state}`, {})
+}
+
+export function deleteById(id: string): Promise<void> {
   return http.delete(`/tenants/${id}`)
 }
